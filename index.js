ae58c3fa2b92dcfe6a5ae94f877e3870b836f9e8
ian <116136272+en-ian-0131@users.noreply.github.com>
2023-04-15 20:42:05 +0800

    自訂404頁面

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ if(process.argv[2] && process.argv[2]==='production'){
 const express = require("express");
 const server = express();
 
-server.set("view engine","ejs")
+// server.set("view engine","ejs")
 
 
 server.get("/",(req,res)=>{
-    // res.send("你好~");
-    res.render("main",{name: 'Ian'})
+    res.send("<h1>Hello world~</h1>");
+    // res.render("main",{name: 'Ian'})
 })
 
 //根目錄要用express.static()
 server.use(express.static("public"))
-server.use(express.static("node_modules/bootstrap/dist"))
+// server.use(express.static("node_modules/bootstrap/dist"))
 
 
 server.use((req,res)=>{
-    res.status(404).send(`<h1>找不到頁面</h1><p>404</p><img src="/imgs/12345.jpeg"/>`)
+    res.status(404).send(`<h1>404 找不到頁面</h1> <img src="./imgs/12345.jpeg" />`)
 })
 
 const port = process.env.PORT || 3000;
 server.listen(port,()=>{
-    console.log(`伺服器正常啟動:${port}`);
+    console.log(`伺服器${port}正常啟動`);
 })
